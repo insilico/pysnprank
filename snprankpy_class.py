29974ac910866d68a36c85ba55a87a9253b7d575
Ahwan Pandey <ahwan-pandey@utulsa.edu>
2010-06-16 13:57:05 -0500

    removed unnecessary attributes and added a test file, also the matrix appers the same way as the input file

--- snprankpy_class.py
+++ snprankpy_class.py
@@ -3,33 +3,14 @@ from __future__ import division
 from numpy import *
 import sys, os, string, math, csv
 
-def transpose(a):
-    return zip(*a)
-
 class DataProperties(object):
     
     def __init__(self, infilename):
         tsv = open(infilename,'r')
         reader = csv.reader(tsv, delimiter="\t")
         
-        data = [row for row in reader]
+        self.data = [row for row in reader]
         
-        tsv.close()
-        
-        data_transpose = transpose(data)
-        
-        # status symbol
-        self.status_key = string.strip(data[0][-1])
-
-        self.num_instances  = len(data) - 1
-        
-        # Create attribute_name -> data dictionary
-        self.attributes = {}
-        for row in data_transpose:
-            self.attributes[row[0]] = row[1:]
-            # remember when constructing data set with sampled attributes
-            # to include status_key at the end
-            
-            
     def pagerank_powermethod(self):
-        matrix = array(self.attributes.values())
+        matrix = array(self.data[1:])
+        print matrix
