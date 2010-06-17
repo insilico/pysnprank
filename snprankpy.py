0bb6b878c5a7b4e79ea5bc1220798a1d1ddc3c6d
Ahwan Pandey <ahwan-pandey@utulsa.edu>
2010-06-17 14:41:18 -0500

    Added command line gamma value input parameter

--- snprankpy.py
+++ snprankpy.py
@@ -5,12 +5,13 @@ import sys, os, string, math, csv
 
 try:
     infilename = sys.argv[1]
+    gamma = sys.argv[2]
 except:
-    print "Usage:", sys.argv[0], "in_all_data"
-    print "Example:", sys.argv[0], "example_data.tab"
+    print "Usage:", sys.argv[0], "in_all_data", "gamma_value"
+    print "Example:", sys.argv[0], "example_data.tab", ".85"
     sys.exit(1)
     
 # Create DataProperties object from data file
 full_data = DataProperties(infilename)
 
-full_data.pagerank_powermethod(.85)
+full_data.pagerank_powermethod(float(gamma))
