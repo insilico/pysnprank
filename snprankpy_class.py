08f677a37edaae9af934aaae2b2118b43e5b0169
Ahwan Pandey <ahwan-pandey@utulsa.edu>
2010-06-30 11:42:26 -0500

    Defined main function, added option parser, defaults to stdin and stdout, added print_to_file() function, powermethod() now returns values

--- snprankpy_class.py
+++ snprankpy_class.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 from __future__ import division
 from numpy import *
-import sys, os, string, math, csv
+import sys, csv, optparse
 
 class DataProperties(object):
     
     def __init__(self, infilename):
-        tsv = open(infilename,'r')
-        reader = csv.reader(tsv, delimiter="\t")
+        #Open file to read
+        reader = csv.reader(infilename, delimiter="\t")
         
         #Store all data
         self.data = [row for row in reader]
@@ -46,8 +46,8 @@ class DataProperties(object):
         #Create a diagional matrix and 
         #a vector of (1-gamma) of size n for the formula
         for i in colsum_nzidx[0]:
-                D[i] = 1/colsum[i]
-                T_nz[i] = T_nz[i] - p
+            D[i] = 1/colsum[i]
+            T_nz[i] = T_nz[i] - p
         
         T = (p * G * D ) + (Gdiag * T_nz) / Gtrace
         T = T.transpose()
@@ -64,6 +64,7 @@ class DataProperties(object):
         #Initialize Values
         converged = False
         
+        #Initiate convergence and SNP ranking
         while(not converged):
             r_old = r
             r = dot(T,r)
@@ -71,8 +72,52 @@ class DataProperties(object):
             r = r/lamb 
             if all((abs(r-r_old))<threshold):
                 converged = True
-        print r
-        print "lambda = ", lamb
-       
                 
+        #Return SNP_rank and diagonal of GAIN matrix
+        return r.reshape(1,n)[0],Gdiag
+    
+    #Output to file function
+    def print_to_file(self, SNPs, snp_rank, ig, output):
+        column_header = ('SNPs','SNP_RANK','IG')
+        #Create list of tuples of SNPs, snp_rank and Eigenvalues
+        temp = zip(SNPs, snp_rank, ig)
+        #Sort the list of tuples by snp_rank, i.e. 2nd value in each tuple
+        sort_temp = sorted(temp, key=lambda item:item[1], reverse =True)
+        #write column headings to file
+        output.write("\t".join(head for head in column_header))
+        #Write values to file
+        for x in range(len(sort_temp)):
+            output.write("\n")
+            output.write("\t".join(str(value) for value in sort_temp[x]))
+        output.close()
+
+#Define main function       
+def main():
+    #Create option parser
+    parser = optparse.OptionParser("usage: %prog -f FILENAME -o OUTPUT -g GAMMA VALUE")
+    #Add options to parser; use defaults if none specified
+    parser.add_option("-f", "--file", dest="infile", help="read data from FILENAME", default=sys.stdin, action ='store')
+    parser.add_option("-o", "--output", dest="outfile", help="output to FILENAME", default=sys.stdout)
+    parser.add_option("-g", "--gamma", dest="gamma", help="gamma value", default=.85)
+    
+    (options, args) = parser.parse_args()
+    
+    #Check to see if filenames are mentioned,
+    #Open to r/w if not
+    if type(options.infile)==type('string'):
+        options.infile = open(options.infile, 'r')
+    if type(options.outfile)==type('string'):
+        options.outfile = open(options.outfile, 'w')
         
+    #Create data object from class
+    full_data = DataProperties(options.infile)
+    
+    #Get SNP_rank and Eigenvalues from powermethod()
+    snprank, IG = full_data.powermethod(float(options.gamma))
+    
+    #Print to file
+    full_data.print_to_file(full_data.SNPs,snprank, IG, options.outfile)
+    
+          
+if __name__ == "__main__":
+    main()
